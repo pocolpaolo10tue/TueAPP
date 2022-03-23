7f1c5060fada693b4c5f75a7239aa99ee761a49d
Lucas <47983556+Lucasmeteenc@users.noreply.github.com>
2022-03-23 14:18:14 +0100

    Map bounds working properly

--- app/src/main/java/com/example/tueapp/MainActivity.java
+++ app/src/main/java/com/example/tueapp/MainActivity.java
@@ -4,22 +4,16 @@ import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.tueapp.databinding.ActivityMainBinding;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
 
@@ -34,15 +28,15 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        SupportMapFragment mapFragment = SupportMapFragment.newInstance();
+
         getSupportFragmentManager()
                 .beginTransaction()
-                .add(R.id.frame_layout, mapFragment)
+                .add(R.id.frame_layout, new MapFragment())
                 .commit();
 
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
             if (item.getItemId() == R.id.map) {
-                replaceFragment(mapFragment);
+                replaceFragment(new MapFragment());
             }
             else if(item.getItemId() == R.id.book) {
                 replaceFragment(new BookFragment());
@@ -63,42 +57,5 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frame_layout, fragment);
         fragmentTransaction.commit();
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        LatLngBounds tueBounds = new LatLngBounds(
-            new LatLng(51.452505317648395, 5.483781791020252),
-            new LatLng(51.4465266596796, 5.4991904032922445)
-        );
-
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(tueBounds.getCenter(),
-                20));
-        int width = getResources().getDisplayMetrics().widthPixels;
-        int height = getResources().getDisplayMetrics().heightPixels;
-
-        int padding = (int) (width * 0.01);
-
-        googleMap.setLatLngBoundsForCameraTarget(tueBounds);
-
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(tueBounds, width, height, padding));
-
-        googleMap.setMinZoomPreference(googleMap.getCameraPosition().zoom);
-
-        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-            @Override
-            public void onMapClick(@NonNull LatLng latLng) {
-                MarkerOptions markerOptions = new MarkerOptions();
-                markerOptions.position(latLng);
-                markerOptions.title(latLng.latitude + ":" + latLng.longitude);
-                googleMap.clear();
-                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
-                        latLng, 10
-                ));
-                googleMap.addMarker((markerOptions));
-
-            }
-        });
-
     }
 }
--- app/src/main/java/com/example/tueapp/MapFragment.java
+++ app/src/main/java/com/example/tueapp/MapFragment.java
@@ -2,11 +2,21 @@ package com.example.tueapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,42 +25,61 @@ import android.view.ViewGroup;
  */
 public class MapFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public MapFragment() {
         // Required empty public constructor
     }
 
-    // TODO: Rename and change types and number of parameters
-    public static MapFragment newInstance(String param1, String param2) {
-        MapFragment fragment = new MapFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        //Initialize view
+        View view = inflater.inflate(R.layout.fragment_maps, container, false);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        //Initialize map fragment
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map);
+
+        //MapAsync
+        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull GoogleMap googleMap) {
+                LatLng one = new LatLng(51.452505317648395, 5.483781791020252);
+                LatLng two = new LatLng(51.4465266596796, 5.4991904032922445);
+
+                LatLngBounds.Builder builder = new LatLngBounds.Builder();
+
+                builder.include(one);
+                builder.include(two);
+
+                LatLngBounds bounds = builder.build();
+
+                int width = getResources().getDisplayMetrics().widthPixels;
+                int height = getResources().getDisplayMetrics().heightPixels;
+
+                int padding = (int) (width * 0.01);
+
+                googleMap.setLatLngBoundsForCameraTarget(bounds);
+
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, width, height, padding));
+
+                googleMap.setMinZoomPreference(googleMap.getCameraPosition().zoom);
+                googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                    @Override
+                    public void onMapClick(@NonNull LatLng latLng) {
+                        MarkerOptions markerOptions = new MarkerOptions();
+                        markerOptions.position(latLng);
+                        markerOptions.title(latLng.latitude + ":" + latLng.longitude);
+                        googleMap.clear();
+                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                                latLng, 10
+                        ));
+                        googleMap.addMarker((markerOptions));
+
+                    }
+                });
+            }
+        });
+        return view;
     }
 }
