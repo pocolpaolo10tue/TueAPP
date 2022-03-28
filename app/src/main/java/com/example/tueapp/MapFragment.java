9119b8e05e5fcc1e4175b5f9df10e4bdc12e9468
lucas <L.w.t.lahaije@student.tue.nl>
2022-03-28 18:15:31 +0200

    Added map locate me and message that locate succedeed

--- app/src/main/java/com/example/tueapp/MapFragment.java
+++ app/src/main/java/com/example/tueapp/MapFragment.java
@@ -1,5 +1,7 @@
 package com.example.tueapp;
 
+import android.annotation.SuppressLint;
+import android.location.Location;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,6 +11,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -18,11 +21,6 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MapFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class MapFragment extends Fragment {
 
     public MapFragment() {
@@ -42,8 +40,27 @@ public class MapFragment extends Fragment {
 
         //MapAsync
         supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+            @SuppressLint("MissingPermission")
             @Override
             public void onMapReady(@NonNull GoogleMap googleMap) {
+                googleMap.setMyLocationEnabled(true);
+                googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+                googleMap.setOnMyLocationClickListener(new GoogleMap.OnMyLocationClickListener() {
+                    @Override
+                    public void onMyLocationClick(@NonNull Location location) {
+                        Toast.makeText(getActivity(), location.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                googleMap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
+                    @Override
+                    public boolean onMyLocationButtonClick() {
+                        Toast.makeText(getActivity(), "location!", Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+                });
+
+
                 LatLng one = new LatLng(51.452505317648395, 5.483781791020252);
                 LatLng two = new LatLng(51.4465266596796, 5.4991904032922445);
 
