0327c99cbc7885342f97caa701bef2c193a48624
Lucas <47983556+Lucasmeteenc@users.noreply.github.com>
2022-03-30 09:29:43 +0200

    Paolos changes to the map

--- app/src/main/java/com/example/tueapp/MainActivity.java
+++ app/src/main/java/com/example/tueapp/MainActivity.java
@@ -5,13 +5,10 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -19,12 +16,13 @@ import androidx.fragment.app.FragmentTransaction;
 
 import com.example.tueapp.databinding.ActivityMainBinding;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+    public static boolean locationPermissionGranted;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    private boolean permission;
     FirebaseAuth mAuth;
 
 
@@ -76,5 +74,66 @@ public class MainActivity extends AppCompatActivity {
         fragmentTransaction.replace(R.id.frame_layout, fragment);
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
+    }
+
+    //Code snippet from https://developer.android.com/training/permissions/requesting
+    /**
+     * Prompts the user for permission to use the device location.
+     */
+    private boolean getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            locationPermissionGranted = true;
+        } else {
+            locationPermissionRequest.launch(new String[] {
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            });
+        }
+        return true;
+    }
+
+    // Register the permissions callback, which handles the user's response to the
+    // system permissions dialog. Save the return value, an instance of
+    // ActivityResultLauncher, as an instance variable.
+    ActivityResultLauncher<String[]> locationPermissionRequest =
+            registerForActivityResult(new ActivityResultContracts
+                            .RequestMultiplePermissions(), result -> {
+                        Boolean fineLocationGranted = null;
+                        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                            fineLocationGranted = result.getOrDefault(
+                                    Manifest.permission.ACCESS_FINE_LOCATION, false);
+                        }
+                        Boolean coarseLocationGranted = null;
+                        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+                            coarseLocationGranted = result.getOrDefault(
+                                    Manifest.permission.ACCESS_COARSE_LOCATION,false);
+                        }
+                        if (fineLocationGranted != null && fineLocationGranted) {
+                            // Precise location access granted.
+                            locationPermissionGranted = true;
+                            finish();
+                            startActivity(getIntent());
+
+                        } else if (coarseLocationGranted != null && coarseLocationGranted) {
+                            // Only approximate location access granted.
+                            locationPermissionGranted = true;
+                            finish();
+                            startActivity(getIntent());
+
+                        } else {
+                            locationPermissionGranted=false;
+                        }
+                    }
+            );
+
+    public static Boolean getMyData() {
+        return locationPermissionGranted;
     }
 }
