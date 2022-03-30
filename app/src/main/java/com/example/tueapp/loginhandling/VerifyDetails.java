b2d7f929871effc745c31bca2e1b679a62d9710d
Lucas <47983556+Lucasmeteenc@users.noreply.github.com>
2022-03-30 14:43:13 +0200

    Improved javadoc and added 2 extra ifs for passnotvalid and namenotvalid

--- app/src/main/java/com/example/tueapp/loginhandling/VerifyDetails.java
+++ app/src/main/java/com/example/tueapp/loginhandling/VerifyDetails.java
@@ -1,20 +1,17 @@
 package com.example.tueapp.loginhandling;
 
-import java.util.Locale;
-
 public class VerifyDetails {
 
     /**
      * validates email.
-     * @param email
+     * @param email the email to validate
      * @return boolean value if email is valid
      */
     public boolean isValidEmail(String email) {
-        Boolean result;
 
-        Boolean faculty_email = email.contains("@tue.nl");
-        Boolean student_email = email.contains("@student.tue.nl");
-        Boolean valid_length = false;
+        boolean faculty_email = email.contains("@tue.nl");
+        boolean student_email = email.contains("@student.tue.nl");
+        boolean valid_length = false;
         if (faculty_email) {
             valid_length = email.length() >= 8;
         } else if (student_email) {
@@ -26,35 +23,35 @@ public class VerifyDetails {
 
     /**
      * validates name.
-     * @param name
+     * @param name the name to validate
      * @return boolean value if name is valid
      */
     public boolean isValidName(String name) {
         name = name.toLowerCase();
 
-        Boolean spaces = name.contains(" ");
-        Boolean min_length = 3 <= name.length();
-        Boolean max_length = 15 >= name.length();
-        Boolean only_letters = name.matches("[a-zA-Z]+");
+        boolean spaces = name.contains(" ");
+        boolean min_length = 3 <= name.length();
+        boolean max_length = 15 >= name.length();
+        boolean only_letters = name.matches("[a-zA-Z]+");
 
         return !spaces && min_length && max_length && only_letters;
     }
 
     /**
      * validates password.
-     * @param password
+     * @param password the password to validate
      * @return boolean value if password is valid
      */
     public boolean isValidPassword(String password) {
-        Boolean correct_length = password.length() >= 6;
-        Boolean contains_number = password.matches(".*\\d+.*");
+        boolean correct_length = password.length() >= 6;
+        boolean contains_number = password.matches(".*\\d+.*");
 
         return contains_number && correct_length;
     }
 
     /**
      * String why email is not valid.
-     * @param email
+     * @param email the email to give reasoning why not valid
      * @return String with information why email is not valid
      * @throws IllegalArgumentException if email is valid
      */
@@ -62,8 +59,8 @@ public class VerifyDetails {
         if (isValidEmail(email)) {
             throw new IllegalArgumentException("This is a valid email.");
         }
-        Boolean faculty_email = email.contains("@tue.nl");
-        Boolean student_email = email.contains("@student.tue.nl");
+        boolean faculty_email = email.contains("@tue.nl");
+        boolean student_email = email.contains("@student.tue.nl");
 
         if (!(faculty_email || student_email)) {
             return "Please use a tu/e email.";
@@ -73,7 +70,7 @@ public class VerifyDetails {
 
     /**
      * String why password is not valid.
-     * @param password
+     * @param password the password to give reasoning why not valid
      * @return String with information why password is not valid
      * @throws IllegalArgumentException if password is valid
      */
@@ -81,18 +78,24 @@ public class VerifyDetails {
         if (isValidPassword(password)) {
             throw new IllegalArgumentException("This is a valid password.");
         }
-        Boolean correct_length = password.length() >= 6;
-        Boolean contains_number = password.matches(".*\\d+.*");
+        boolean correct_length = password.length() >= 6;
+        boolean contains_number = password.matches(".*\\d+.*");
 
         if (!correct_length) {
             return "Password must have at least 6 characters.";
         }
-        return "Password must contain atleast one number.";
+        if (contains_number) {
+            return "Password must contain at least one number.";
+        }
+        else {
+            return "password is not valid for unknown reason try another password";
+        }
+
     }
 
     /**
      * String why name is not valid.
-     * @param name
+     * @param name the name to give reasoning why not valid
      * @return String with information why name is not valid
      * @throws IllegalArgumentException if name is not valid
      */
@@ -100,10 +103,10 @@ public class VerifyDetails {
         if (isValidName(name)) {
             throw new IllegalArgumentException("This is a valid name.");
         }
-        Boolean spaces = name.contains(" ");
-        Boolean min_length = 4 <= name.length();
-        Boolean max_length = 15 >= name.length();
-        Boolean only_letters = name.matches("[a-zA-Z]+");
+        boolean spaces = name.contains(" ");
+        boolean min_length = 4 <= name.length();
+        boolean max_length = 15 >= name.length();
+        boolean only_letters = name.matches("[a-zA-Z]+");
 
         if (spaces) {
             return "Name cannot contain spaces.";
@@ -111,7 +114,9 @@ public class VerifyDetails {
             return "Name is too short.";
         } else if (!max_length) {
             return "Name is too long.";
+        } else if (only_letters) {
+            return "Name may only contain letters";
         }
-        return "Name can only contain letters.";
+        return "Name is not valid for unknown reason try another name.";
     }
 }
