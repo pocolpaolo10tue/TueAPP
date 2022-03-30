64c0184919e74b1649ddd415d96de58e3edfc8f9
pocolpaolo10 <75096052+pocolpaolo10@users.noreply.github.com>
2022-03-30 15:22:12 +0200

    Function

--- app/src/main/java/com/example/tueapp/MainActivity.java
+++ app/src/main/java/com/example/tueapp/MainActivity.java
@@ -30,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        getLocationPermission();
+
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
